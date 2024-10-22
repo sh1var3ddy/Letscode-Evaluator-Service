72fa0eb54af9f677646ca416b2cb6fa7cc1653ab
sh1var3ddy <srajamuri@gmail.com>
2024-10-22 15:27:31 -0400

    Added routes and controllers

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import serverConfig from "./config/serverConfig";
+import apiRouter from "./routes";
 console.log("hello");
 console.log("wow");
 const app = express();
+
+app.use("/api",apiRouter);
 app.listen(serverConfig.PORT,()=>{
     console.log(`App listening on port ${serverConfig.PORT}`);
 })
---
+++ src/routes/v1/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { pingCheck } from "../../controllers/pingController";
+
+const v1Router = express.Router();
+
+v1Router.get("/ping",pingCheck);
+export default v1Router;
